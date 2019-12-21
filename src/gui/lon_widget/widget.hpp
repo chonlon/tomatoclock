42bd6b0411b3e9ac75348a35796e56828e54ee7d
chonlon <lonelycooler.xcl@gmail.com>
2019-12-21 18:08:22 +0800

    <fix>:优化widget有背景图的时候的resize的性能。

--- src/gui/lon_widget/widget.hpp
+++ src/gui/lon_widget/widget.hpp
@@ -23,7 +23,7 @@ namespace lon {
 /// 只允许对centerWidget和botttomWidget进行操作(包括替换).
 ///	</summary>
 class Widget : public QWidget {
-Q_OBJECT
+    Q_OBJECT
 private:
     TitleBar* title_bar_;
 
@@ -39,8 +39,12 @@ private:
     QPixmap* pixmap_;
 
     qint64 last_time_epoch_{0};
-    // 这个属性主要是在设置了背景图以后每次resize的时候都会resize背景图, 可以适当设置间隔一定的时间来提高resize的流畅度.
-    qint64 reset_background_time_{100};
+    // 这个属性主要是在设置了背景图以后每次resize的时候都会resize背景图,
+    // 可以适当设置间隔一定的时间来提高resize的流畅度.
+    qint64 reset_background_time_{50};
+
+    QTimer timer_;
+    bool block_one_;
 
 protected:
     QWidget* center_widget_;
@@ -48,8 +52,7 @@ protected:
 
 private:
     // 禁用setlayout, 只允许操作centerWidget和botttomWidget.
-    virtual void setLayout(QLayout*) {
-    }
+    virtual void setLayout(QLayout*) {}
 
     void initLayout() {
         p_layout_ = new QGridLayout(this);
@@ -68,6 +71,7 @@ private:
 
         ok_button_ = new Button(bottom_bar_);
         cancel_button_ = new Button(bottom_bar_);
+
 
         // 这里由于pushbutton不能使text在icon下面显示.
         // 所以只好使用样式表了
@@ -100,36 +104,25 @@ private:
 
     void initConnect() {
         connect(ok_button_, SIGNAL(clicked()), this, SLOT(onOkButtonClicked()));
-        connect(cancel_button_,
-                SIGNAL(clicked()),
-                this,
-                SLOT(onCancelButtonClicked()));
-        connect(title_bar_,
-                SIGNAL(minimizeButtonClicked()),
-                this,
-                SIGNAL(minimizeButtonClicked()));
-        connect(title_bar_,
-                SIGNAL(maximizeButtonClicked()),
-                this,
-                SIGNAL(maximizeButtonClicked()));
-        connect(title_bar_,
-                SIGNAL(closeButtonClicked()),
-                this,
-                SIGNAL(closeButtonClicked()));
+        connect(cancel_button_, SIGNAL(clicked()), this, SLOT(onCancelButtonClicked()));
+        connect(title_bar_, SIGNAL(minimizeButtonClicked()), this, SIGNAL(minimizeButtonClicked()));
+        connect(title_bar_, SIGNAL(maximizeButtonClicked()), this, SIGNAL(maximizeButtonClicked()));
+        connect(title_bar_, SIGNAL(closeButtonClicked()), this, SIGNAL(closeButtonClicked()));
+        connect(&timer_, &QTimer::timeout, [this]() {
+            this->setBackground(pixmap_);
+            block_one_ = false;
+        });
     }
 
     void initWidgets() {
         pixmap_ = nullptr;
+        timer_.setSingleShot(true);
         QPalette palette;
-        palette.setBrush(this->backgroundRole(),
-                         QBrush(QColor(255, 255, 255, 30)));
+        palette.setBrush(this->backgroundRole(), QBrush(QColor(255, 255, 255, 30)));
         title_bar_->setPalette(palette);
     }
 
 protected:
-
-
-
     void resizeEvent(QResizeEvent* event) override {
         QWidget::resizeEvent(event);
         sizeChanged(event);
@@ -138,20 +131,30 @@ protected:
         if (pixmap_->isNull())
             return;
         const auto current_epoch = QDateTime::currentMSecsSinceEpoch();
-        qDebug() << event->size().width() << " h: " << event->size().height() << "\n";
-        if(last_time_epoch_ == 0) {
-            last_time_epoch_ = current_epoch;
-        } else if(current_epoch - last_time_epoch_ >= reset_background_time_){
-            //this->setBackground(pixmap_);
+        // qDebug() << event->size().width() << " h: " << event->size().height() << "\n";
+        // this->setBackground(pixmap_);
+        if (last_time_epoch_ == 0) {
+            this->setBackground(pixmap_);
+        } else if (current_epoch - last_time_epoch_ >= reset_background_time_) {
+            this->setBackground(pixmap_);
+            qDebug() << current_epoch << "direct";
+        } else {
+            if (!block_one_) {
+                timer_.start(reset_background_time_);
+                block_one_ = true;
+            } else {
+                timer_.stop();
+                this->setBackground(pixmap_);
+                block_one_ = false;
+            }
         }
+        last_time_epoch_ = current_epoch;
     }
 
 public:
-    explicit Widget(QWidget* parent = nullptr,
-                    TitleBar::Buttons status = TitleBar::ALL)
+    explicit Widget(QWidget* parent = nullptr, TitleBar::Buttons status = TitleBar::ALL)
         : QWidget(parent) {
-        this->setWindowFlags(Qt::FramelessWindowHint |
-            Qt::WindowMinimizeButtonHint);
+        this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowMinimizeButtonHint);
         title_bar_ = new TitleBar(this, status);
         center_widget_ = new QWidget();
         bottom_bar_ = new QWidget();
@@ -163,14 +166,11 @@ public:
         initConnect();
     }
 
-    explicit Widget(QWidget* center_widget,
-                    QWidget* bottom_bar,
-                    QWidget* parent = nullptr)
+    explicit Widget(QWidget* center_widget, QWidget* bottom_bar, QWidget* parent = nullptr)
         : QWidget(parent) {
         assert(center_widget && "pointer cannot be null");
         assert(bottom_bar && "pointer cannot be null");
-        this->setWindowFlags(Qt::FramelessWindowHint |
-            Qt::WindowMinimizeButtonHint);
+        this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowMinimizeButtonHint);
 
         title_bar_ = new TitleBar(this);
 
@@ -197,7 +197,9 @@ public:
     }
 
     /// <summary> 返回中间栏的widget指针. </summary>
-    QWidget* centerWidget() const { return center_widget_; }
+    QWidget* centerWidget() const {
+        return center_widget_;
+    }
 
     /// <summary> 设置自定义的bottombar. </summary>
     /// 注意: 重新设置bottomBar会导致默认生成的按钮的信号与槽的失效.
@@ -223,20 +225,26 @@ public:
 
         delete center_widget_;
         center_widget_ = widget;
+
         if (center_widget_)
             p_layout_->addWidget(center_widget_, 1, 0);
         return true;
     }
 
     /// <summary> 返回底部栏的widget指针. </summary>
-    QWidget* bottomBar() const { return bottom_bar_; }
-    virtual void setTitle(const QString& title) { title_bar_->setTitle(title); }
+    QWidget* bottomBar() const {
+        return bottom_bar_;
+    }
+    virtual void setTitle(const QString& title) {
+        title_bar_->setTitle(title);
+    }
 
     virtual void setTitleIcon(const QIcon& icon) {
         title_bar_->setTitleIcon(icon);
     }
 
     // this class will take pixmap's ownship
+
     virtual void setTitleBackground(QPixmap* pixmap) {
         title_bar_->setBackground(pixmap);
     }
@@ -245,13 +253,12 @@ public:
         size_girp_enabled = true;
         size_grip_ = new QSizeGrip(this);
         size_grip_->setFixedSize(size_grip_->sizeHint());
-        p_layout_->addWidget(size_grip_,
-                             2,
-                             1,
-                             Qt::AlignRight | Qt::AlignBottom);
+        p_layout_->addWidget(size_grip_, 2, 1, Qt::AlignRight | Qt::AlignBottom);
     }
 
-    virtual bool sizeGripEnabled() { return size_girp_enabled; }
+    virtual bool sizeGripEnabled() {
+        return size_girp_enabled;
+    }
 
     // this class will take pixmap's ownship
     virtual void setBackground(QPixmap* pixmap) {
@@ -263,12 +270,9 @@ public:
         }
         //设置窗口的背景
         QPalette palette = this->palette();
-        auto brush = QBrush(pixmap->scaled(this->size(),
-            Qt::IgnoreAspectRatio,
-            Qt::SmoothTransformation));
-        palette.setBrush(
-            this->backgroundRole(),
-            std::move(brush));
+        auto brush =
+            QBrush(pixmap->scaled(this->size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+        palette.setBrush(this->backgroundRole(), std::move(brush));
         this->setPalette(palette);
         pixmap_ = pixmap;
     }
@@ -315,30 +319,28 @@ private slots:
 };
 
 class ShadowWindow : public QWidget {
-Q_OBJECT
+    Q_OBJECT
 public:
-    explicit ShadowWindow(QWidget* contentWidget, QWidget* parent = nullptr)
-        : QWidget(parent) {
-        setWindowFlags(Qt::FramelessWindowHint);    // 去掉边框
-        setAttribute(Qt::WA_TranslucentBackground); // 背景透明
+    explicit ShadowWindow(QWidget* contentWidget, QWidget* parent = nullptr) : QWidget(parent) {
+        setWindowFlags(Qt::FramelessWindowHint);     // 去掉边框
+        setAttribute(Qt::WA_TranslucentBackground);  // 背景透明
 
         // 添加阴影
-        QGraphicsDropShadowEffect* shadowEffect =
-            new QGraphicsDropShadowEffect(contentWidget);
+        QGraphicsDropShadowEffect* shadowEffect = new QGraphicsDropShadowEffect(contentWidget);
         shadowEffect->setColor(Qt::lightGray);
-        shadowEffect->setBlurRadius(6); // 阴影的大小
+        shadowEffect->setBlurRadius(6);  // 阴影的大小
         shadowEffect->setOffset(0, 0);
         contentWidget->setGraphicsEffect(shadowEffect);
 
         // 添加到窗口中
         QVBoxLayout* layout = new QVBoxLayout(this);
         layout->addWidget(contentWidget);
-        layout->setContentsMargins(4, 4, 4, 4); // 注意和阴影大小的协调
+        layout->setContentsMargins(4, 4, 4, 4);  // 注意和阴影大小的协调
         setLayout(layout);
         connect(contentWidget,
-                SIGNAL(sizeChanged(QResizeEvent *)),
+                SIGNAL(sizeChanged(QResizeEvent*)),
                 this,
-                SLOT(sizeChanged(QResizeEvent *)));
+                SLOT(sizeChanged(QResizeEvent*)));
     }
 
 public slots:
@@ -346,6 +348,6 @@ public slots:
         this->resize(event->size().width() + 8, event->size().height() + 8);
     }
 };
-} // namespace lon
+}  // namespace lon
 
 #endif
