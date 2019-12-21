3eceedad3f955521a4e61a4f5a2627bce3478a63
chonlon <lonelycooler.xcl@gmail.com>
2019-12-21 19:26:40 +0800

    <fix>:remove unused file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,7 +1,7 @@
 #include "../gui/clock_main_widget/clockmainwidget.h"
 #include <QApplication>
 #include <QDialog>
-#include "../gui/mainwindow.h"
+
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
